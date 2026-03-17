63960e9274fb8cbb64e06eaa51868f8e77bf9eed
Timothy Lam <84857278+Timbob0456@users.noreply.github.com>
2026-03-16 22:31:26 -0400

    Add files via upload

---
+++ BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/BinaryAPIController.java
@@ -0,0 +1,97 @@
+package com.ontariotechu.sofe3980U;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BinaryAPIController {
+
+	@GetMapping("/add")
+	public String addString(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
+                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
+		Binary number1=new Binary (operand1);
+		Binary number2=new Binary (operand2);
+        return  Binary.add(number1,number2).getValue();
+		// http://localhost:8080/add?operand1=111&operand2=1010
+	}
+
+@GetMapping("/multiply")
+public String multiply(
+        @RequestParam String operand1,
+        @RequestParam String operand2) {
+
+    Binary op1 = new Binary(operand1);
+    Binary op2 = new Binary(operand2);
+    return op1.multiply(op2).getValue();
+}
+
+@GetMapping("/and")
+public String and(
+        @RequestParam String operand1,
+        @RequestParam String operand2) {
+
+    Binary op1 = new Binary(operand1);
+    Binary op2 = new Binary(operand2);
+    return op1.and(op2).getValue();
+}
+
+@GetMapping("/or")
+public String or(
+        @RequestParam String operand1,
+        @RequestParam String operand2) {
+
+    Binary op1 = new Binary(operand1);
+    Binary op2 = new Binary(operand2);
+    return op1.or(op2).getValue();
+}
+	
+	@GetMapping("/add_json")
+	public BinaryAPIResult addJSON(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
+                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
+		Binary number1=new Binary (operand1);
+		Binary number2=new Binary (operand2);
+        return  new BinaryAPIResult(number1,"add",number2,Binary.add(number1,number2));
+		// http://localhost:8080/add?operand1=111&operand2=1010
+	}
+
+	@GetMapping("/multiply_json")
+public BinaryAPIResult multiply_json(
+        @RequestParam String operand1,
+        @RequestParam String operand2) 
+{
+
+    Binary op1 = new Binary(operand1);
+    Binary op2 = new Binary(operand2);
+    Binary result = op1.multiply(op2);
+
+    return new BinaryAPIResult(op1, "*", op2, result);
+}
+
+@GetMapping("/and_json")
+public BinaryAPIResult and_json(
+        @RequestParam String operand1,
+        @RequestParam String operand2) 
+{
+
+    Binary op1 = new Binary(operand1);
+    Binary op2 = new Binary(operand2);
+    Binary result = op1.and(op2);
+
+    return new BinaryAPIResult(op1, "&", op2, result);
+}
+
+@GetMapping("/or_json")
+public BinaryAPIResult or_json(
+        @RequestParam String operand1,
+        @RequestParam String operand2) 
+{
+
+    Binary op1 = new Binary(operand1);
+    Binary op2 = new Binary(operand2);
+    Binary result = op1.or(op2);
+
+    return new BinaryAPIResult(op1, "|", op2, result);
+}
+
+}
